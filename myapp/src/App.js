154f61829c557b343cd62d1ca7203908505a3996
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-22 22:30:00 -0800

    button

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,60 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import { Button } from "@mui/material";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          color="primary"
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="outlined">Material-UI ボタン</Button>
+        <CheckboxExample />
+        <ButtonGroup>
+          <Button
+            startIcon={<SaveIcon />}
+            onClick={() => alert("Hello!")}
+            size="large"
+            // disabled
+            variant="contained"
+            color="primary"
+          >
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            onClick={() => alert("Hello!")}
+            size="large"
+            // disabled
+            variant="contained"
+            color="secondary"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
