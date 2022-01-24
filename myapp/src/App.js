cfcd1221bbe02a9c31d0f062209c4f4d4da36786
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-23 18:21:58 -0800

    fin

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,7 +13,19 @@ import TextField from "@mui/material/TextField";
 // import { makeStyles } from "@mui/material/styles";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+import green from "@mui/material/colors/green";
 import orange from "@mui/material/colors/orange";
+import "@fontsource/roboto";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+import AppBar from "@mui/material/AppBar";
+import ToolBar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 // const useStyles = makeStyles({
 //   root: {
@@ -27,9 +39,18 @@ import orange from "@mui/material/colors/orange";
 // });
 
 const theme = createTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    },
+  },
   palette: {
     primary: {
-      main: orange[500],
+      main: green[500],
+    },
+    secondary: {
+      main: orange[400],
     },
   },
 });
@@ -61,42 +82,76 @@ function CheckboxExample() {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          {/* <ButtonStyled /> */}
-          <TextField
-            variant="filled"
-            color="secondary"
-            type="email"
-            label="Your Email"
-            placeholder="test@test.com"
-          />
-          <CheckboxExample />
-          <ButtonGroup>
-            <Button
-              startIcon={<SaveIcon />}
-              onClick={() => alert("Hello!")}
-              size="large"
-              // disabled
-              variant="contained"
-              color="primary"
-            >
-              Save
-            </Button>
-            <Button
-              startIcon={<DeleteIcon />}
-              onClick={() => alert("Hello!")}
-              size="large"
-              // disabled
-              variant="contained"
-              color="primary"
-            >
-              Discard
-            </Button>
-          </ButtonGroup>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Container maxWidth="xs">
+        <div className="App">
+          <header className="App-header">
+            {/* <ButtonStyled /> */}
+            <AppBar color="secondary">
+              <ToolBar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">MUI Theming</Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ marginLeft: 2 }}
+                >
+                  Login
+                </Button>
+              </ToolBar>
+            </AppBar>
+            <Typography variant="h2" component="div" sx={{ marginTop: 4 }}>
+              Welcome to MUI
+            </Typography>
+            <Typography variant="subtitle">
+              Learn how to use Material UI
+            </Typography>
+            <TextField
+              variant="filled"
+              color="secondary"
+              type="email"
+              label="Your Email"
+              placeholder="test@test.com"
+            />
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+            </Grid>
+            <CheckboxExample />
+            <ButtonGroup>
+              <Button
+                startIcon={<SaveIcon />}
+                onClick={() => alert("Hello!")}
+                size="large"
+                // disabled
+                variant="contained"
+                color="primary"
+              >
+                Save
+              </Button>
+              <Button
+                startIcon={<DeleteIcon />}
+                onClick={() => alert("Hello!")}
+                size="large"
+                // disabled
+                variant="contained"
+                color="primary"
+              >
+                Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
     </ThemeProvider>
   );
 }
